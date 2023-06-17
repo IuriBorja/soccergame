5d4abdd9c9cbac5218f8efe30911965a035e5525
emanuel perdigao casa <emanuelfsperdigao@gmail.com>
2023-06-17 21:34:51 +0100

    make some changes in main game and player

--- ourSoccerGame/src/org/academiadecodigo/oursoccergame/Main.java
+++ ourSoccerGame/src/org/academiadecodigo/oursoccergame/Main.java
@@ -1,7 +1,14 @@
 package org.academiadecodigo.oursoccergame;
 
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
 public class Main {
     public static void main(String[] args) {
 
+        //Create game
+        Game game = new Game();
+        game.start();
+        Picture background = new Picture(10, 10, "resources/background.png");
+        background.draw();
     }
 }
